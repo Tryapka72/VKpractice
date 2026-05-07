4ed905a86cea0d395f4bd90e6c2c75758d716a86
Tryapka72 <salfetka72@yandex.ru>
2026-05-07 12:52:25 +0300

    initiall comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import styles from "../App.module.css";
+
 export default function App() {
-  return (
-    <>
-      <h1 className="header">MiniStore</h1>
-      <p>Первый экран</p>
-    </>
-  );
-}
+    return (
+        <>
+        <h1 className={styles.header}>MiniStore</h1>
+        <main>
+            <section className={styles.card}>
+                <h1>Товар 1</h1>
+                <p>Описание</p>
+                <span>24/09</span>
+            </section>
+            <section className={styles.card}>
+                <h1>Товар 2</h1>
+                <p>Описание</p>
+                <span>24/09</span>
+            </section>
+            <section className={styles.card}>
+                <h1>Товар 3</h1>
+                <p>Описание</p>
+                <span>24/09</span>
+            </section>
+        </main>
+        </>
+    )
+};
